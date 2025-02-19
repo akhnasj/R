71af71d536a577a9995cb4542c84f1cf0cc9c552
Akhna <akhnasj1612@gmail.com>
2025-02-19 23:25:01 +0530

    Installed Packages

---
+++ installed_packages.R
@@ -0,0 +1,23 @@
+
+
+# Installed Packages
+
+install.packages("shiny")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("xts")
+install.packages("tidyverse")
+install.packages("quantmod")
+install.packages("plotly")
+install.packages("flexdashboard")
+install.packages("writexl")
+install.packages("openxlsx")
+install.packages("learnr")
+install.packages("pryr")
+
+
+
+
+library(shiny)
+library(pryr)
+pryr::mem_used()
